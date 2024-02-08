fff25f15b812301e4e628397403981e6427db930
marcus <marcus@marcus>
2024-02-07 19:13:50 -0600

    Improved #8

--- Regression Ex13 Basic Generalized Models in Ecology.R
+++ Regression Ex13 Basic Generalized Models in Ecology.R
@@ -28,4 +28,25 @@ library(viridis)
 index <- 0:600 * .1
 points(index,predict(Ex3MDL,data.frame(ROCK = index)), type = "l", col =viridis(1)[1], lwd = 3)
 
+Data <- Data[,-6]
 
+createTestSet <- function(i,limit=TRUE){
+  i2 <- (i*(!limit) - 1) + limit * 5
+  returnV <- c()
+  for(number in 1:i2){
+    returnV <- c(returnV, combn(2:i,number,simplify=FALSE))
+  }
+  return(returnV)
+}
+
+toTest <- createTestSet(length(Data[,c(-1)]), limit=FALSE)
+
+aics <- c()
+for(vars in toTest){
+  print(vars)
+  print(colnames(as.data.frame(Data[,c(vars)])))
+  print(reformulate( paste("s(",colnames(Data)[vars],")" ), "Data$SR"))
+  aics <- c(aics, AIC(gam(reformulate( paste("s(",colnames(as.data.frame(Data[,c(vars)])),")" ), "Data$SR"), data = Data)))
+}
+
+MinAICModel <- gam(reformulate( paste("s(",colnames(as.data.frame(Data[,toTest[which.min(aics)][[1]]])), ")"), "Data$SR"), data = Data)
